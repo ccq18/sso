97eceb5348beb6e6afcecd1e8605bbe8f8b2edec
刘荣冬 <rongdong.liu@aihuishou.com>
2019-08-07 11:26:53 +0800

    no message

--- doc/readme.md
+++ doc/readme.md
@@ -1,27 +1,25 @@
 # 单点登录项目
 ## 跳转登陆
-/login?sigin=xxxxx&return_url=xxxx
+/login?sign=xxxxx&return_url=xxxx
 登陆成功后会返回到return_url页面 并带上token=xxx
+
+
+## 用户信息获取接口
+/auth-token?token=xxxx
+
+返回信息:默认信息
+```
+{
+id:1,
+name:"aaa",
+    //其他自定义信息
+}
+```  
 
 ## jsonp登录
 /login/jsonp?sigin=xxxxx&callback=xxxx&username=xxx&password=xxx
 登陆
 
-## 用户信息获取接口
-/userinfo?token=xxxx
-
-返回信息:默认信息
-```
-{
-"user":{
-    },
-"roles":[
-    "role1",
-    "role2",
-        ],
-    //其他自定义信息
-}
-```  
 ps：返回内容应该根据业务需要做自定义扩展
 ## 签名
 
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AuthController@showLoginForm')->name('login');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/auth-token', 'HomeController@authToken')->name('authToken');
 Route::any('/logout', 'AuthController@logout')->name('logout');
